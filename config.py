6b221ec83c18a7c1ba92b96c1e57935dfdb30e9a
Oscar Knagg <oscar@knagg.co.uk>
2018-11-03 13:10:13 +0000

    Added global EPSILON variable

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ PATH = os.path.dirname(os.path.realpath(__file__))
 
 DATA_PATH = None
 
+EPSILON = 1e-8
+
 if DATA_PATH is None:
     raise Exception('Configure your data folder location in config.py before continuing!')
 
